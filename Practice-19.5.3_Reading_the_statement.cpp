3794a94745086137f4e414bfe35242164954986e
Alexandr <sasha_stepanov94@mail.ru>
2024-06-30 00:17:58 +0300

    Correct third

--- Practice-19.5.3_Reading_the_statement.cpp
+++ Practice-19.5.3_Reading_the_statement.cpp
@@ -12,13 +12,70 @@ int moneyControl(std::string moneyStr) {
   }
   return stoi(moneyStr);
 }
+
+void dateControl(std::string date) {
+  int sizeMoneyStr = date.length();
+  int countDots = 0;
+  std::string day = "", month = "", year = "";
+
+  bool control = true;
+
+  for (int i = 0; i < sizeMoneyStr && control; i++) {
+    if (date[i] == '.') {
+      countDots++;
+    }
+
+    if (!isdigit(date[i]) && date[i] != '.') {
+      control = false;
+      break;
+    }
+
+    if (countDots == 0 && isdigit(date[i])) {
+      day += date[i];  
+    }
+    if (countDots == 1 && isdigit(date[i])) {
+      month += date[i];
+    } 
+    if (countDots == 2 && isdigit(date[i])) {
+      year += date[i];
+    }
+  }
+  
+  if (countDots != 2) control = false;
+  int dd = stoi(day);
+  int mm = stoi(month);
+  int yyyy = stoi(year);
+
+  if (1 > mm || mm > 12) {
+    control = false;
+  } else if (1 > dd || dd > 31) {
+    control = false;
+  } else if (yyyy < 0) {
+    control = false;
+  } else if (mm == 2) {
+    if ((yyyy % 400 == 0) ||
+        (yyyy % 4 == 0 && yyyy % 100 != 0)) {
+      if (dd > 29) control = false;
+    } else {
+      if (dd > 28) control = false;
+    }
+  } else if (mm == 4 || mm == 6 || mm == 9 ||
+             mm == 11) {
+    if (dd > 30) control = false;
+  } else {
+    if (dd > 31) control = false;
+  }
+
+  if (!control) std::cerr << "Error in date\n";
+}
+
+
 std::string outputAlignment(std::string str) {
   std::string alignment = "";
-  if (str.length() < 10) {  
-    for (int count = 0; count < 10 - str.length(); count++)
-      alignment += " ";
+  if (str.length() < 10) {
+    for (int count = 0; count < 10 - str.length(); count++) alignment += " ";
   }
-  return alignment;  
+  return alignment;
 }
 
 int main() {
@@ -32,10 +89,12 @@ int main() {
   while (!notebook.eof()) {
     notebook >> name >> surname >> moneyStr >> date;
 
-    std::cout << name << "\t" << surname << outputAlignment(surname) << "\t" 
+    std::cout << name << "\t" << surname << outputAlignment(surname) << "\t"
               << moneyStr << "\t" << date << "\n";
 
     money = moneyControl(moneyStr);
+    dateControl(date);
+
     if (maxMoney < money) {
       maxMoney = money;
       maxNameSurname = name + " " + surname;
@@ -43,6 +102,6 @@ int main() {
     if (money > -1) sum += money;
   }
 
-  std::cout << "\nTotal amount of payments: " << sum << "\n\n";
-  std::cout << maxNameSurname << "'s with max payments " << maxMoney;
+  std::cout << "\nTotal payment amount: " << sum << "\n\n";
+  std::cout << maxNameSurname << "'s with max payments " << maxMoney << "\n";
 }
